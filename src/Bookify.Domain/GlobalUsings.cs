fc60232f073e878bc259c41cbf050d50392cb479
Denis Semko <53062219+DenisSemko@users.noreply.github.com>
2023-08-16 12:47:31 +0300

    Add Review Entity

--- src/Bookify.Domain/GlobalUsings.cs
+++ src/Bookify.Domain/GlobalUsings.cs
@@ -1,6 +1,8 @@
 global using Bookify.Domain.Abstractions;
-global using MediatR;
-global using Bookify.Domain.Users.Events;
 global using Bookify.Domain.Apartments;
+global using Bookify.Domain.Bookings;
+global using Bookify.Domain.Reviews.Events;
 global using Bookify.Domain.Shared;
+global using Bookify.Domain.Users.Events;
+global using MediatR;
 global using System.Diagnostics.CodeAnalysis;
