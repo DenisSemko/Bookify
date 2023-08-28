c4676059be3033297850d3f4ded5acfb1ca1de4b
Denis Semko <53062219+DenisSemko@users.noreply.github.com>
2023-08-28 18:03:16 +0300

    Implement Presentation Layer without migrations data

---
+++ src/Bookify.API/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Bookify.Application.Apartments.SearchApartments;
+global using Bookify.Application.Bookings.GetBooking;
+global using Bookify.Application.Bookings.ReserveBooking;
+global using Bookify.Application;
+global using Bookify.Infrastructure;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
